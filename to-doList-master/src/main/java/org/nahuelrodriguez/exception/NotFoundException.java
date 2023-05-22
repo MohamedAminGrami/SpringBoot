40b9d88ceb6adc7dd37e777e27f5c317cd9d3c49
Mohamed Amin Grami <mohamedgrami32@gmail.com>
2023-05-22 16:18:19 +0100

    Delete

--- to-doList-master/src/main/java/org/nahuelrodriguez/exception/NotFoundException.java
+++
@@ -1,4 +0,0 @@
-package org.nahuelrodriguez.exception;
-
-public class NotFoundException extends RuntimeException {
-}
--- to-doList-master/src/main/java/org/nahuelrodriguez/service/MessagingQueueProducer.java
+++
@@ -1,9 +0,0 @@
-package org.nahuelrodriguez.service;
-
-import org.nahuelrodriguez.request.dto.NewTodoItemRequest;
-
-import java.util.Collection;
-
-public interface MessagingQueueProducer {
-    void sendMessage(final Collection<NewTodoItemRequest> dtos);
-}
